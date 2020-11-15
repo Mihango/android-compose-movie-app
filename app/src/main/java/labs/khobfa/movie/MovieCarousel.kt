850961d0bf5a47b8ab5a4a8d5dbea565fcafc58d
Kennedy Kariuki <kennedy.kariuki@tracom.co.ke>
2020-11-15 15:14:30 +0300

    enables scrollable background with current image

--- app/src/main/java/labs/khobfa/movie/MovieCarousel.kt
+++ app/src/main/java/labs/khobfa/movie/MovieCarousel.kt
@@ -15,18 +15,24 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawOpacity
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.lerp
 import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorStop
 import androidx.compose.ui.graphics.VerticalGradient
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.ConfigurationAmbient
-import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.platform.*
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlin.math.abs
+import kotlin.math.roundToInt
 
 data class Movie(
     val title: String,
@@ -116,12 +122,20 @@ val posterAspectRatio = .674f
 
 @Composable
 fun Screen() {
-    val screenSize = ConfigurationAmbient.current.screenWidthDp.dp
+    val configuration = ConfigurationAmbient.current
+    val density = DensityAmbient.current
+    val screenWidth = configuration.screenWidthDp.dp
+    val screenWidthPx = with(density) { screenWidth.toPx() }
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenHeightPx = with(density) { screenHeight.toPx() }
+
     var offset by remember { mutableStateOf(0f) }
     val scrollController = rememberScrollableController { delta ->
         offset += delta
         delta
     }
+
+    val indexFraction = -1 * offset / screenWidthPx
 
     Box(
         modifier = Modifier
@@ -133,9 +147,11 @@ fun Screen() {
             )
     ) {
         movies.forEachIndexed { index, movie ->
+            val opacity = if (indexFraction.roundToInt() == index) 1f else 0f
             CoilImage(
                 data = movie.bgUrl,
                 modifier = Modifier
+                    .drawOpacity(opacity)
                     .fillMaxWidth()
                     .aspectRatio(posterAspectRatio)
             )
@@ -144,19 +160,22 @@ fun Screen() {
         Spacer(
             modifier = Modifier
                 .align(alignment = Alignment.BottomEnd)
-                .background(VerticalGradient(listOf(Color.White, Color.Transparent), 0f, 1f))
+                .verticalGradient(0f to Color.Transparent, 0.3f to Color.White, 1f to Color.White)
                 .fillMaxWidth()
-                .fillMaxHeight(0.3f)
+                .fillMaxHeight(0.6f)
         )
 
         movies.forEachIndexed { index, movie ->
+            val center = screenWidthPx * index
+            val distFromCenter = abs(center - offset) / screenWidthPx
             MoviePoster(
                 index = index,
-                screenSize = screenSize,
+                screenSize = screenWidth,
                 movie = movie,
                 modifier = Modifier
-                    .offset(getX = { screenSize * index + offset.dp }, getY = { 0.dp })
-                    .width(screenSize * .75f)
+                    .offset(getX = { center + offset }, getY = { 0f })
+                    .width(screenWidth * .75f)
+                    .align(Alignment.BottomCenter)
             )
         }
     }
@@ -177,7 +196,7 @@ fun MoviePoster(index: Int, screenSize: Dp, movie: Movie, modifier: Modifier = M
             data = movie.posterUrl,
             contentScale = ContentScale.Crop,
             modifier = modifier
-                .fillMaxWidth()
+                .width(screenSize * 0.6f)
                 .aspectRatio(posterAspectRatio)
                 .clip(RoundedCornerShape(10.dp))
         )
@@ -240,8 +259,8 @@ fun BuyTicket(onClick: () -> Unit) {
 }
 
 fun Modifier.offset(
-    getX: () -> Dp,
-    getY: () -> Dp,
+    getX: () -> Float,
+    getY: () -> Float,
     rtlAware: Boolean = true
 ) = this then object : LayoutModifier {
     override fun MeasureScope.measure(
@@ -251,10 +270,49 @@ fun Modifier.offset(
         val placeable = measurable.measure(constraints)
         return layout(placeable.width, placeable.height) {
             if (rtlAware) {
-                placeable.placeRelative(getX().toIntPx(), getY().toIntPx())
+                placeable.placeRelative(getX().roundToInt(), getY().roundToInt())
             } else {
-                placeable.place(getX().toIntPx(), getY().toIntPx())
+                placeable.place(getX().roundToInt(), getY().roundToInt())
             }
         }
     }
 }
+
+fun Modifier.verticalGradient(vararg colors: ColorStop) =
+    this then object : DrawModifier, InspectableParameter {
+
+        // naive cache outline calculation if size is the same
+        private var lastSize: Size? = null
+        private var lastBrush: Brush? = null
+
+        override fun ContentDrawScope.draw() {
+            drawRect()
+            drawContent()
+        }
+
+        private fun ContentDrawScope.drawRect() {
+            var brush = lastBrush
+            if (size != lastSize || brush == null) {
+                brush = VerticalGradient(
+                    *colors,
+                    startY = 0f,
+                    endY = size.height
+                )
+                lastSize = size
+                lastBrush = brush
+            }
+
+            brush?.let { drawRect(brush = brush, alpha = 1f) }
+        }
+
+        override val nameFallback = "verticalGradient"
+
+        override val valueOverride: Any?
+            get() = colors
+
+        override val inspectableElements: Sequence<ParameterElement>
+            get() = sequenceOf(
+                ParameterElement("color", colors)
+            )
+    }
+
